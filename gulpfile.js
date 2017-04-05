97d937561491a106ceaacdb2552f6fcd48a5aa2c
Konovalov-Sergey <konovalov.s.e@gmail.com>
2017-04-05 18:28:40 +0300

    Jersy

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,6 @@ var gulp = require('gulp'),
   concatCss = require('gulp-concat-css'),
   concat = require('gulp-concat'),
   pngquant = require('imagemin-pngquant'),
-
   rigger = require('gulp-rigger'),
   rimraf = require('rimraf'),
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,51 @@
-var main = function() {};
-	
-	$(function() {
-		$( "#accordion" ).accordion();
+$(function() {
+//accordion
+	$( "#accordion" ).accordion();
+//slider
+	$('.bxslider').bxSlider({
+	mode: 'horizontal',
+	slideWidth: 350,
+	minSlides: 2,
+	maxSlides: 3,
+	moveSlides: 1,
+	slideMargin: 5
+	});
+//popup
+	$( "#tel" ).dialog({
+		autoOpen: false,
+		show: {
+			effect: "blind",
+			duration: 1000
+		},
+		hide: {
+			effect: "explode",
+			duration: 1000
+		}
 	});
 
-	$(function() {
-		$('.bxslider').bxSlider({
-		mode: 'horizontal',
-		slideWidth: 350,
-		minSlides: 2,
-		maxSlides: 3,
-		moveSlides: 1,
-		slideMargin: 5
-		});
+	$( "#opener" ).on( "click", function() {
+		$( "#tel" ).dialog( "open" );
 	});
-
-	function initMap() {
-		var myLatLng = {
-			lat:40.742110, 
-			lng: -74.185128
-		};
-		var map = new google.maps.Map(document.getElementById('map'), {
-			center: myLatLng,
-			zoom: 12,
-			scrollwheel: false,
-			draggable: false
-		});
-		var image = {
-			url: '../img/map-marker.png'
-		};
-		var marker = new google.maps.Marker({
-			position: myLatLng,
-			map: map,
-			icon: image
-		});
+});
+//map
+function initMap() {
+	var myLatLng = {
+		lat:40.742110, 
+		lng: -74.185128
 	};
-
-$(document).ready(main);
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: myLatLng,
+		zoom: 12,
+		scrollwheel: false,
+		draggable: false
+	});
+	var image = {
+		url: '../img/map-marker.png'
+	};
+	var marker = new google.maps.Marker({
+		position: myLatLng,
+		map: map,
+		icon: image
+	});
+};
+	
